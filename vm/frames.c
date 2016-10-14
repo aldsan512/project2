bc1bd410eb812f74b4500b9d852cf35c82e09563
aldsan512 <aldsan_512@yahoo.com>
2016-10-15 00:10:01 +0100

    still adding functionality to frame table

--- vm/frames.c
+++ vm/frames.c
@@ -5,30 +5,34 @@
 #include <round.h>
 #include <stdio.h>
 #include <string.h>
-
+#include "threads/vaddr.h"
+#include "threads/thread.h"
 static FrameEntry** frameTable;
-void initFrame(size_t numFrames){
+static int numFrames;
+void initFrame(size_t numF){
+	numFrames=numF;
 	frameTable=(FrameEntry**)malloc(sizeof(FrameEntry*)*numFrames);
 	for(int i=0;i < numFrames;i++){
 		frameTable[i]=(FrameEntry*)malloc(sizeof(FrameEntry));
-		frameTable[i]->framePT=palloc_get_page(PAL_USER);
+		frameTable[i]->framePT=palloc_get_page(PAL_USER| PAL_ZERO);
 		frameTable[i]->isModified=false;
 		frameTable[i]->owner=NULL;
 	}
 	
 }
-void* getFrame(struct Thread* owner){
+void* getFrame(struct thread* owner){
 	for(int i=0;i<numFrames;i++){
 		if(frameTable[i]->owner==NULL){
 			frameTable[i]->owner=owner;
-			return frameTable[i]->framePt;
+			return frameTable[i]->framePT;
 		}
 	}
 }
-bool releaseFrame(struct Thread* owner){
+bool releaseFrame(struct thread* owner){
 	for(int i=0;i<numFrames;i++){
 		if(frameTable[i]->owner->tid==owner->tid){
 			frameTable[i]->owner=NULL;
+			memset (frameTable[i]->framePT,0,PGSIZE);
 			return true;
 		}
 	}
